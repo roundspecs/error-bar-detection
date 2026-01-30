128dba2ca4ad8ed55c45c2b4af4d2266f1e15578
Zarif Muhtasim <2502.zarif@gmail.com>
2026-01-30 10:52:25 +0600

    Refactor image generation: update DPI selection to use configuration constants and improve background and spine settings

--- config.py
+++ config.py
@@ -2,8 +2,10 @@
 MAX_BARHEIGHT = 430.0
 MINIMUM_PHANTOM_DISTANCE = 15
 
+# Generation
 PLOT_TYPE_PROBS = {
     "linegraph": 0.65,
     "barchart": 0.30,
     "boxplot": 0.05,
 }
+DPI_OPTIONS = [72, 100, 150, 200]
--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,17 +1,18 @@
 import uuid
 import random
 from matplotlib import pyplot as plt
-from config import PLOT_TYPE_PROBS
+from config import DPI_OPTIONS, PLOT_TYPE_PROBS
 
-def generate_image(output_dir: str):
+def generate_image():
   """Generates one image"""
-
   id = str(uuid.uuid4())
-
   w = random.randint(8, 15)
   h = random.randint(6, 12)
-  dpi = random.choice([72, 100, 150, 200])
+  dpi = random.choice(DPI_OPTIONS)
   fig, ax = plt.subplots(figsize=(w, h), dpi=dpi)
+
+  set_background(ax)
+  set_spines(ax)
 
   dice = random.random()
   if dice < PLOT_TYPE_PROBS["linegraph"]:
@@ -21,9 +22,27 @@ def generate_image(output_dir: str):
   else:
     data = generate_boxplot(ax)
 
-  fig.savefig(f"{output_dir}/images/{id}.png")
-  plt.close(fig)
-  # TODO: Save data to file
+  return fig, data
+
+def set_background(ax):
+  if random.random() > 0.7:
+      ax.set_facecolor(random.choice(["#f0f0f0", "#e0e0e0", "#ebebeb"]))  # Light gray
+      ax.grid(True, linestyle="--", alpha=0.5)
+      ax.set_axisbelow(True)
+  else:
+      ax.set_facecolor("white")
+      if random.random() > 0.5:
+          ax.grid(True, linestyle=":", alpha=0.4, color="gray")
+
+def set_spines(ax):
+  if random.random() > 0.4:
+      ax.spines["top"].set_visible(False)
+      ax.spines["right"].set_visible(False)
+  if random.random() > 0.8:  # Occasional floating axes
+      ax.spines["left"].set_position(("outward", 10))
+      ax.spines["bottom"].set_position(("outward", 10))
+      ax.spines["top"].set_visible(False)
+      ax.spines["right"].set_visible(False)
 
 def generate_linegraph(ax):
   pass
