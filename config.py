c57ccd490b66a0f3eff8bdea2776564f578d6644
Zarif Muhtasim <2502.zarif@gmail.com>
2026-01-30 17:16:47 +0600

    chore: Increase `PATCH_W` configuration value from 32 to 48.

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ GEN_MAX_LABEL_PX_FACTOR = 1.5     # Clamp labels to factor * height
 
 # Detection
 PATCH_H = 800  # 800px height covers ±400px from center, which is safe.
-PATCH_W = 32   # Error bar caps are usually 10-20px wide, so 32px is safe.
+PATCH_W = 48   # Error bar caps are usually 10-20px wide, so 32px is safe.
 
 # Training
 NUM_GENERATED_IMAGES = 3000 
